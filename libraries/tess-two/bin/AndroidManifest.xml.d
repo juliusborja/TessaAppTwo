52dd72b61ee1f4a0ea07974e70f5bbb9cbad12a4
JuliusChristopherBorja <37961839+juliusborja@users.noreply.github.com>
2018-08-23 23:58:37 +0800

    Initial commit

---
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Julius\Documents\Yumi\tess-two-master\tess-two-master\tess-two\bin\AndroidManifest.xml : \
+C:\Users\Julius\Documents\Yumi\tess-two-master\tess-two-master\tess-two\AndroidManifest.xml \
